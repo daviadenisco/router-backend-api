a9e5533c04cd2deb95f0d95e71628edaf9226761
Davia DeNisco <davia622@gmail.com>
2018-02-20 16:32:15 -0500

    initial commit

---
+++ migrations/20180220161900_create_table_router_play_backend_api_db.js
@@ -0,0 +1,11 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('routerPlay', (table) => {
+  table.increments();
+  table.string('origin', 64);
+  })
+};
+
+exports.down = function(knex, Promise) {
+
+};
